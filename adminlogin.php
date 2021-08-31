7afc94d692a2e1521b305c74207bb9f80cf24a91
KiranBendkoli1 <bendkolikiran@kbtcoe.org>
2021-08-31 18:03:09 +0530

    register and login func

--- adminlogin.php
+++ adminlogin.php
@@ -12,6 +12,43 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#login").click(function () {
+
+                email = $("#email").val();
+                password = $("#password").val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "pcheck.php",
+                    data: "email=" + email + "&password=" + password,
+                    success: function (html) {
+                        if (html == 'true') {
+
+                            $("#add_err2").html('<div class="alert alert-success"> <strong>Authenticated</strong></div>');
+
+                            window.location.href = "userdashboard.php";
+
+                        } else if (html == 'false') {
+                            $("#add_err2").html('<div class="alert alert-danger"><strong>Authentication</strong> failed </div>');                    
+
+                        } else {
+                            $("#add_err2").html('<div class="alert alert-danger"> <strong>Error</strong> processing request. Please try again. </div>');
+                        }
+                    },
+                    beforeSend: function () {
+                        $("#add_err2").html("loading...");
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
+
+
 </head>
 <body>
     <!--Navbar-->
@@ -29,7 +66,7 @@
             <a class="nav-link" href="index.php">User Login</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="registration.php">User Registration</a>
+            <a class="nav-link" href="register.php">User Registration</a>
             </li>
             <li class="nav-item active">
             <a class="nav-link" href="adminlogin.php">Admin Login</a>
@@ -44,7 +81,9 @@
                 <p>Library Infor</p>
             </div>
             <div class="col-lg-6"  style="align-items: center;">
+                <hr>
                 <h1 style="text-align:center">Admin Login Form</h1>
+                <hr>
                 <form action="">
                     <div class="form-group">
                         <label for="email"></label>Email Address
--- index.php
+++ index.php
@@ -4,19 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LMS - Home</title>
+    <title>BloodBank- Home</title>
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#login").click(function () {
+
+                email = $("#email").val();
+                password = $("#password").val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "checkuser.php",
+                    data: "email=" + email + "&password=" + password,
+                    success: function (html) {
+                        if (html == 'true') {
+
+                            $("#add_err2").html('<div class="alert alert-success"> <strong>Authenticated</strong></div>');
+
+                            window.location.href = "userdashboard.php";
+
+                        } else if (html == 'false') {
+                            $("#add_err2").html('<div class="alert alert-danger"><strong>Authentication</strong> failed </div>');                    
+
+                        } else {
+                            $("#add_err2").html('<div class="alert alert-danger"> <strong>Error</strong> processing request. Please try again. </div>');
+                        }
+                    },
+                    beforeSend: function () {
+                        $("#add_err2").html("loading...");
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
 
 </head>
 <body>
     <!--Navbar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Library Management System</a>
+    <a class="navbar-brand" href="#">BloodBank Management System</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mynavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -29,7 +65,7 @@
             <a class="nav-link active" href="index.php">User Login</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="registration.php">User Registration</a>
+            <a class="nav-link" href="register.php">User Registration</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="adminlogin.php">Admin Login</a>
@@ -44,11 +80,14 @@
     <div class="container-fluid" style="padding: 80px; background-color: #e3f2fd;height: 100vh;">
         <div class="row">
             <div class="col-lg-6" >
-                <p>Library Infor</p>
+                <p>Blood Bank</p>
             </div>
             <div class="col-lg-6"  style="align-items: center;">
+                <hr>
                 <h1 style="text-align:center">User Login Form</h1>
-                <form action="index.php" method="POST">
+                <hr>
+                <div id="add_err2"></div>
+                <form role="form" method="POST">
                     <div class="form-group">
                         <label for="email"></label>Email Address
                         <input type="email" class="form-control" id="email"> 
@@ -60,6 +99,10 @@
                     <button type="submit" class="btn btn-primary" id="login">login</button>
     
                 </form>
+
+                <div class="form-group col-lg-12">
+                        <a href="register.php"><button type="submit" class="btn btn-default">Not a Member? Register here</button></a>
+                    </div>
             </div>
         </div>
     </div>    
